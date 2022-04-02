2408399c6ac625f68fe52d6404c13292f8b471a1
B4tiste <batiste.laloi@gmail.com>
2022-04-02 18:57:12 +0200

    Partie 3 - ajout des accessoires

--- S6/Exam - tlw/exoTesla/script.js
+++ S6/Exam - tlw/exoTesla/script.js
@@ -2,10 +2,25 @@ function remplir_accessoires() {
     console.log("Démarage de la fonction au chargement de la page");
 
     fetch("./accessoires.json")
-    .then((response) => response.json())
-    .then((data) => {
-        let tbl = data;
-        console.log(tbl);
-    })
-    .catch((error) => console.log(error));
+        .then((response) => response.json())
+        .then((data) => {
+
+            let template = document.querySelector("#template_accessoires");
+            let grid = document.querySelector("#grid-container");
+
+            for (const a of data.tableau_accessoires) {
+                let clone = document.importNode(template.content, true);
+
+                (newContent = clone.firstElementChild.innerHTML
+                    .replace(/{{accessoires-image}}/g, a.image)
+                    .replace(/{{accessoires-titre}}/g, a.titre)
+                    .replace(/{{accessoires-texte}}/g, a.texte)),
+                    (clone.firstElementChild.innerHTML = newContent);
+
+                grid.appendChild(clone);
+            }
+        })
+        .catch((error) => console.log(error));
 }
+
+remplir_accessoires();
