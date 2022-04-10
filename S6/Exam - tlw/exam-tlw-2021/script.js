678d12d26fb50e32bf52d30ce3b133a7a2e9f60d
B4tiste <batiste.laloi@gmail.com>
2022-04-10 20:20:32 +0200

    Fin de l'annale de web CHATEAU

--- S6/Exam - tlw/exam-tlw-2021/script.js
+++ S6/Exam - tlw/exam-tlw-2021/script.js
@@ -1,8 +1,13 @@
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() { scrollFunction() };
+window.onscroll = function () {
+    scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
@@ -13,3 +18,37 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+let content = document.getElementById("visites");
+for (const link of content.getElementsByTagName("a")) {
+    link.style.color = "#666";
+    link.style.textDecoration = "none";
+}
+
+function remplirActu() {
+    console.log("Démarage de la fonction au chargement de la page");
+
+    fetch("./actu.json")
+        .then((response) => response.json())
+        .then((data) => {
+
+            let template = document.querySelector("#my_template");
+            let grid = document.querySelector("#grid-container");
+
+            for (const a of data.tableau_actu) {
+                let clone = document.importNode(template.content, true);
+
+                (newContent = clone.firstElementChild.innerHTML
+                    .replace(/{{actu-image}}/g, a.image)
+                    .replace(/{{actu-titre}}/g, a.titre)
+                    .replace(/{{actu-texte}}/g, a.texte)
+                    .replace(/{{actu-bouton}}/g, a.bouton)),
+                    (clone.firstElementChild.innerHTML = newContent);
+
+                grid.appendChild(clone);
+            }
+        })
+        .catch((error) => console.log(error));
+}
+
+remplirActu()
