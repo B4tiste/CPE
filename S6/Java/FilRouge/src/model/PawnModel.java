f154ba3237ce11c4dc148785ff041a9769247e64
B4tiste <batiste.laloi@gmail.com>
2022-04-11 14:09:47 +0200

    Avancement methodes PawnModel.java

--- S6/Java/FilRouge/src/model/PawnModel.java
+++ S6/Java/FilRouge/src/model/PawnModel.java
@@ -1,97 +1,128 @@
 package model;
 
-
 import java.util.LinkedList;
 import java.util.List;
 
 import nutsAndBolts.PieceSquareColor;
 
-public class PawnModel implements PieceModel{
+public class PawnModel implements PieceModel {
 
-	private Coord coord;
-	private PieceSquareColor pieceColor;
+    private Coord coord;
+    private PieceSquareColor pieceColor;
 
-	public PawnModel(Coord coord, PieceSquareColor pieceColor) {
-		super();
+    public PawnModel(Coord coord, PieceSquareColor pieceColor) {
+        super();
 
-		// TODO Atelier 1
+        // TODO Atelier 1
 
-	}
+        this.coord = coord;
+        this.pieceColor = pieceColor;
 
-	@Override
-	public char getColonne() {
-		char colonne = ' ';
-		
-		// TODO Atelier 1
+    }
 
-		return colonne;
-	}
+    @Override
+    public char getColonne() {
+        char colonne = ' ';
 
-	@Override
-	public int getLigne() {
-		int ligne = -1;
-		
-		// TODO Atelier 1
+        // TODO Atelier 1
 
-		return ligne;
-	}
+        return colonne;
+    }
 
-	@Override
-	public boolean hasThisCoord(Coord coord) {
-		boolean hasThisCoord = false;
-		
-		// TODO Atelier 1
+    @Override
+    public int getLigne() {
+        int ligne = -1;
 
-		return hasThisCoord;
-	}
+        // TODO Atelier 1
 
-	@Override
-	public PieceSquareColor getPieceColor() {
-		PieceSquareColor color = null;
-		
-		// TODO Atelier 1
+        return ligne;
+    }
 
-		return color;	
-	}
+    @Override
+    public boolean hasThisCoord(Coord coord) {
+        boolean hasThisCoord = false;
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		String st = null;
+        // TODO Atelier 1
 
-		// TODO Atelier 1
+        return hasThisCoord;
+    }
 
-		return st;
-	}
+    @Override
+    public PieceSquareColor getPieceColor() {
+        PieceSquareColor color = null;
 
-	@Override
-	public void move(Coord coord) {
+        // TODO Atelier 1
 
-		// TODO Atelier 1
+        return color;
+    }
 
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        String st = null;
 
-	@Override
-	public boolean isMoveOk(Coord targetCoord, boolean isPieceToCapture) {
-		boolean ret = false;
+        switch (pieceColor) {
+            case BLACK:
+                st = "[B" + coord + "]";
+                break;
 
-		// TODO Atelier 1
+            case WHITE:
+                st = "[W[" + coord + "]]";
+                break;
 
-		return ret;
-	}
+            default:
+                break;
+        }
 
-	@Override
-	public List<Coord> getCoordsOnItinerary(Coord targetCoord) {
+        return st;
+    }
 
-		List<Coord> coordsOnItinery = new LinkedList<Coord>(); 
+    @Override
+    public void move(Coord coord) {
 
-		// TODO Atelier 2
+        this.coord = coord;
 
-		return coordsOnItinery;
-	}
+    }
 
-	
+    @Override
+    public boolean isMoveOk(Coord targetCoord, boolean isPieceToCapture) {
+        boolean ret = false;
+
+        String alpha = "abcdefghij";
+        int id = alpha.indexOf(targetCoord.getColonne());
+
+        if (isPieceToCapture) {
+            if ((Math.abs(id - alpha.indexOf(this.coord.getColonne())) == 2)
+                    && Math.abs(targetCoord.getLigne() - this.coord.getLigne()) == 2) {
+                ret = true;
+            } else {
+                ret = false;
+            }
+
+        } else {
+            if ((Math.abs(id - alpha.indexOf(this.coord.getColonne())) == 1)
+                    && Math.abs(targetCoord.getLigne() - this.coord.getLigne()) == 1) {
+                ret = true;
+            } else {
+                ret = false;
+            }
+        }
+
+        return ret;
+    }
+
+    @Override
+    public List<Coord> getCoordsOnItinerary(Coord targetCoord) {
+
+        List<Coord> coordsOnItinery = new LinkedList<Coord>();
+
+        // TODO Atelier 2
+
+        return coordsOnItinery;
+    }
+
 }
-
