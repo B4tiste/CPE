1e83bb826806f4ef8de36e0f519f1a2f231ecb7d
b4tiste <batiste.laloi@gmail.com>
2022-09-07 17:25:33 +0200

    Code ex6 : compteur de penchés à gauche

--- S7/Archi Réseau/Intro iot/microbit-samples/source/main.cpp
+++ S7/Archi Réseau/Intro iot/microbit-samples/source/main.cpp
@@ -1,37 +1,27 @@
 #include "MicroBit.h"
 
-
 MicroBit uBit;
 MicroBitThermometer termo;
 
-void onButton(MicroBitEvent e)
-{
-    if (e.source == MICROBIT_ID_BUTTON_A)
-        uBit.display.scroll("A");
-        
-
-    if (e.source == MICROBIT_ID_BUTTON_B)
-        uBit.display.scroll("B"); 
-}
 
 int main()
 {
     // Initialise the micro:bit runtime.
     uBit.init();
 
-    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_EVT_ANY, onButton);
-    uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_EVT_ANY, onButton);
+    int cpt = 0;
 
-    // Insert your code here!
-    int temp = termo.getTemperature();
-    if(temp > 30)
+    while(true)
     {
-        ManagedString display = "tro cho:" + ManagedString(temp) + "C";
-        uBit.display.scroll(display.toCharArray());
-    }
-    else
-    {
-        uBit.display.scroll(temp);
+        int x = uBit.accelerometer.getRoll();
+        if (x < 0)
+        {
+            cpt++;
+        }
+        
+        uBit.display.scroll(cpt, 50);
+
+        uBit.sleep(500);
     }
 
     // If main exits, there may still be other fibers running or registered event handlers etc.
