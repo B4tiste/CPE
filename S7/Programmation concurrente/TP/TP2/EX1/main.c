e1cea47c1d2b62d52e4d100f5726b0dd3cfc22ed
b4tiste <batiste.laloi@gmail.com>
2022-09-09 17:33:55 +0200

    avancement tp

--- S7/Programmation concurrente/TP/TP2/EX1/main.c
+++ S7/Programmation concurrente/TP/TP2/EX1/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-    
+    if(!fork())
+    {
+        execlp("gcc", "gcc", "test.c", "-o", "test.out", NULL);
+    }
+    execlp("ls", "ls", "-al", NULL);
+
+    return 0;
 }
